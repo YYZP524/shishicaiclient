b0d050d85bccb1acbe3c3e5a077be836dcdcda92
cloudtimesoft <499675898@qq.com>
2016-10-31 17:46:16 +0800

    yonhuzhce

--- shishicaiclient/LeftEll.xaml.cs
+++ shishicaiclient/LeftEll.xaml.cs
@@ -28,37 +28,37 @@ namespace shishicaiclient
         {
 
         }
-        public void create_lab(string content, int colortype)
+        public void create_lab(string content, int colortype)//创建圆ell 设置宽高
         {
 
-            Ellipse ell = new Ellipse();
+            Ellipse ell = new Ellipse();//新建“ell”圆
 
             ell.Width = 22;
 
             ell.Height = 22;
 
-            Label lab = new Label();
+            Label lab = new Label();//新建”lab“lable
 
-            lab.Content = content;
+            lab.Content = content;//lab内容
 
-            if (colortype == 0)
+            if (colortype == 0)//判断
             {
-                ell.Fill = Brushes.OrangeRed;
-                lab.Foreground = Brushes.White;
-                lab.Margin = new Thickness(2, -2, 0, 0);
+                ell.Fill = Brushes.OrangeRed;//为0时圆ell为橘红色
+                lab.Foreground = Brushes.White;//为0时lab数字为白色
+                lab.Margin = new Thickness(2, -2, 0, 0);//数字位置
             }
 
             else
             {
 
-                ell.Fill = Brushes.LightGray;
-                lab.Margin = new Thickness(0, -2, 0, 0);
+                ell.Fill = Brushes.LightGray;//为1时圆ell为亮灰色，lab内数字默认为黑色就不写出来
+                lab.Margin = new Thickness(0, -2, 0, 0);//位置
 
             }
 
-            maincanvas.Children.Add(ell);
+            maincanvas.Children.Add(ell);//把画好的圆放在 maincanvas上
 
-            maincanvas.Children.Add(lab);
+            maincanvas.Children.Add(lab);//把定义好的lab放在圆上
 
 
 

59cbf60ebdd490afdae5f70a70c852d4ab4a7167
cloudtimesoft <499675898@qq.com>
2016-11-10 17:39:00 +0800

    2016-11-10-17-38

--- shishicaiclient/Progress.xaml.cs
+++ shishicaiclient/Progress.xaml.cs
@@ -27,49 +27,50 @@ namespace shishicaiclient
 
         public void create_progress(int colortype,int element,int element_count,string content)//colortype 0 红 1蓝 2绿 element 占比个数 element_count 总数 content 内容
         {
-            Rectangle rectborder = new Rectangle();
-            rectborder.Width = 300;
-            rectborder.Height = 20;
-            rectborder.StrokeThickness = 2;
-            rectborder.SnapsToDevicePixels = true;
+
+                Rectangle rectborder = new Rectangle();
+                rectborder.Width = 300;
+                rectborder.Height = 20;
+                rectborder.StrokeThickness = 1;
+                rectborder.SnapsToDevicePixels = true;
 
 
-            Rectangle rect = new Rectangle();
-            rect.Width = element / element_count * 300;
-            rect.Height = 20;
-            rect.SnapsToDevicePixels = true;
+                Rectangle rect = new Rectangle();
+                rect.Width = float.Parse(element.ToString()) / float.Parse(element_count.ToString()) * 300d;
+                rect.Height = 20;
+                rect.SnapsToDevicePixels = true;
 
-            if (colortype == 0)
-            {
-                rectborder.Stroke = Brushes.OrangeRed;
-                rect.Stroke = Brushes.OrangeRed;
-            }
-            else if (colortype == 1)
-            {
-                rectborder.Stroke = Brushes.DodgerBlue;
-                rect.Stroke = Brushes.DodgerBlue;
-            }
-            else
-            {
-                rectborder.Stroke = Brushes.LimeGreen;
-                rect.Stroke = Brushes.LimeGreen;
-            }
+                if (colortype == 0)
+                {
+                    rectborder.Stroke = Brushes.OrangeRed;
+                    rect.Fill = Brushes.OrangeRed;
+                }
+                else if (colortype == 1)
+                {
+                    rectborder.Stroke = Brushes.DodgerBlue;
+                    rect.Fill = Brushes.DodgerBlue;
+                }
+                else
+                {
+                    rectborder.Stroke = Brushes.LimeGreen;
+                    rect.Fill = Brushes.LimeGreen;
+                }
 
-            rect.Margin = new Thickness(45, 5, 0, 0);
-            rectborder.Margin = new Thickness(45, 5, 0, 0);
-            maincanvas.Children.Add(rectborder);
-            maincanvas.Children.Add(rect);
-            typelab.Content = content;
-            presslab.Content = element / element_count * 100 + "%";
+                rect.Margin = new Thickness(45, 5, 0, 0);
+                rectborder.Margin = new Thickness(45, 5, 0, 0);
+                maincanvas.Children.Add(rectborder);
+                maincanvas.Children.Add(rect);
+                typelab.Content = content+" "+element;
+                presslab.Content = (int)(float.Parse(element.ToString()) / float.Parse(element_count.ToString()) * 100f) + "%";
 
 
 
-            DoubleAnimation wid = new DoubleAnimation();
-            wid.From = 0;
-            wid.To = element / element_count * 300;
-            wid.Duration = TimeSpan.FromSeconds(1);
-            rect.BeginAnimation(Rectangle.WidthProperty, wid);
-            
+                DoubleAnimation wid = new DoubleAnimation();
+                wid.From = 0;
+                wid.To = float.Parse(element.ToString()) / float.Parse(element_count.ToString()) * 300f;
+                wid.Duration = TimeSpan.FromSeconds(1);
+                rect.BeginAnimation(Rectangle.WidthProperty, wid);
+
 
 
         }
